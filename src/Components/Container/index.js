092ec01eb9fb5d63ba89a84c9ac5b1042420a889
Anmol varma <anmol_varma@github.com>
2018-02-28 02:22:50 +0530

    Starting the question listing screen

--- src/Components/Container/index.js
+++ src/Components/Container/index.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import LoginBox from '../LoginBox';
+import QuestionList from '../QuestionList';
 import './container.css';
 
 class Container extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showScreen: 1,
+    };
+  }
   render() {
-    return (
-      <div className="Container-wrapper">
-        <LoginBox />
-      </div>
-    );
+    const changeScreen = (screenNumber) => {
+      this.setState({
+        showScreen: screenNumber,
+      });
+    };
+    if (this.state.showScreen === 1) {
+      return (
+        <div className="Container-wrapper">
+          <LoginBox changeScreen={(screenNumber) => {
+            changeScreen(screenNumber);
+          }}
+          />
+        </div>
+      );
+    } else if (this.state.showScreen === 2) {
+      return (
+        <QuestionList
+          username={this.state.username}
+          changeScreen={(screenNumber) => {
+          changeScreen(screenNumber);
+        }}
+        />
+      );
+    }
   }
 }
 
--- src/Components/LoginBox/index.js
+++ src/Components/LoginBox/index.js
@@ -1,30 +1,70 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
 import './loginbox.css';
 
-const LoginBox = () => (
-  <div className="LoginBox-wrapper">
-    <div className="LoginBox-welcome">
-      <div className="LoginBox-welcome-message">Welcome</div>
-      <div className="LoginBox-welcome-message">to</div>
-      <div className="LoginBox-welcome-appName">Quizzy</div>
-    </div>
-    <div className="LoginBox-login">
-      <div className="LoginBox-form">
-        <div className="LoginBox-title">Login</div>
-        <div className="LoginBox-inputArea">
-          <div className="Username-message">Username</div>
-          <div className="Username-message Username-inputField-wrapper">
-            <input type="text" className="Username-inputField" />
-          </div>
-          <div className="LoginBox-button-wrapper">
-            <button className="LoginBox-login-button">Login</button>
+
+const identifyUserAndRedirect = (props, username) => {
+  if (username.trim() === '') {
+    window.alert('Enter valid username');
+  } else {
+    fetch('/users', {
+      method: 'POST',
+      body: JSON.stringify({ username }),
+    }).then(response => response.json()).then(() => {
+      props.changeScreen(1);
+    });
+  }
+};
+
+class LoginBox extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+    };
+  }
+
+  render() {
+    return (
+      <div className="LoginBox-wrapper">
+        <div className="LoginBox-welcome">
+          <div className="LoginBox-welcome-message">Welcome</div>
+          <div className="LoginBox-welcome-message">to</div>
+          <div className="LoginBox-welcome-appName">Quizzy</div>
+        </div>
+        <div className="LoginBox-login">
+          <div className="LoginBox-form">
+            <div className="LoginBox-title">Login</div>
+            <div className="LoginBox-inputArea">
+              <div className="Username-message">Username</div>
+              <div className="Username-message Username-inputField-wrapper">
+                <input
+                  type="text"
+                  className="Username-inputField"
+                  onChange={(event) => {
+                  this.setState({
+                    username: event.target.value,
+                  }, () => {
+                    console.log(this.state.username);
+                  });
+                }}
+                />
+              </div>
+              <div className="LoginBox-button-wrapper">
+                <button
+                  className="LoginBox-login-button"
+                  onClick={() => {
+                    identifyUserAndRedirect(this.props, this.state.username);
+                  }}
+                >Login
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 // LoginBox.propTypes = {
 //   value: PropTypes.string.isRequired,
